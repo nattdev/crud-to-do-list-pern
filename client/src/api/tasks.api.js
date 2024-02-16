82c8a0b99e86621937c6e283243eb8a9c792c215
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-02-16 01:51:45 -0500

    Implement client-side DELETE request

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const getTasksRequest = async () => {
     return await axios.get("http://localhost:4000/tasks");
 }
+
+export const deleteTasksRequest = async (id) => {
+    return await axios.delete(`http://localhost:4000/tasks/${id}`);
+}
