80d79cb59aea6b4d3b5c3bf751e0eb36680afb19
nattdev <110750471+nattdev@users.noreply.github.com>
2024-02-18 20:42:13 -0500

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 import {config} from './config.js';
 
 export const pool = new pg.Pool({
-    user: process.env.DB_HOST || config.DB_USER,
+    user: process.env.DB_USER || config.DB_USER,
     password: process.env.DB_PASSWORD || config.DB_PASSWORD,
     host: process.env.DB_HOST || config.DB_HOST,
     port: process.env.DB_PORT || config.DB_PORT,
