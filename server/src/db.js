c9790afe3b48ad4134bb51f8b7356bb3fbc62557
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 15:06:32 -0500

    Implement dotenv to database connection

--- server/src/db.js
+++ server/src/db.js
@@ -1,17 +1,26 @@
 /*import pg from 'pg';
 import {config} from './config.js';*/
-
+require('dotenv').config()
 const pg = require('pg');
 // const { config } = require('./config.js');
 
-const pool = new pg.Pool({
-		connectionString: process.env.POSTGRES_URL ,
-		//user: process.env.DB_USER || "postgres",
-    //password: process.env.DB_PASSWORD || "123",
-    //host: process.env.DB_HOST || "localhost",
-    //port: process.env.DB_PORT || 5432,
-    //database: process.env.DB_DATABASE || "tasksdb"
-});
+let dbConfig;
+
+if (process.env.NODE_ENV === 'development') {
+  dbConfig = {
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE
+  };
+} else {
+  dbConfig = {
+    connectionString: process.env.POSTGRES_URL,
+  };
+}
+
+const pool = new pg.Pool(dbConfig);
 
 module.exports = { pool };
 
