10ff92a323128c7fc52a8d7bc7411fc6074b7c82
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 18:04:55 -0500

    Create ModalDelete to validate the delete action

---
+++ client/src/components/ModalDelete.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { useTasks } from "./TaskContext";
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: {xs: 200, sm: 400},
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+function ModalDelete({ task }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const { tasks, deleteTask } = useTasks();
+
+    return (
+        <div>
+            <Button color={"warning"} variant="contained" onClick={handleOpen}>DELETE</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        ¿Estás seguro de que deseas eliminar esta tarea?
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Esta acción no se puede deshacer.
+                    </Typography>
+                    <Button sx={{mt : 2}} color={"warning"} variant="contained" onClick={() => deleteTask(task.id)}>DELETE</Button>
+                </Box>
+            </Modal>
+        </div>
+    )
+}
+
+export default ModalDelete;
