f9284ba17a9e0666763b525cb54c74b7578a81c1
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 21:22:38 -0500

    Display date and task id to TaskCard

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -11,8 +11,24 @@ export default function TaskCard({ task }) {
         navigate(`/edit/${task.id}`);
     }
 
+    const isoDateString = task.datecreated;
+    const date = new Date(isoDateString);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    const formattedDate = date.toLocaleString("en-EN", options);
+
     return (
         <Box border={1} m={2} p={2} sx={{maxWidth : 250}}>
+            <Box sx={{display: "flex", flexDirection: "column", gap: "1rem"}}>
+            <Typography variant="body1" sx={{textWrap: "nowrap"}}>Task #{task.id}</Typography>
+            <Typography  mb={1} sx={{textAlign: "left"}} variant="caption">{formattedDate}</Typography>
+            </Box>
             <Typography variant="h3">{task.title}</Typography>
             <Typography variant="body1" py={2}>{task.description}</Typography>
             <Stack direction="row" spacing={1}>
