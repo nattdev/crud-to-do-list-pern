9d772ac3a1dbf3573e47a6331b08c80b0b4bb5e7
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 21:23:45 -0500

    Add text presentation to Home Page View

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,8 +2,14 @@ import { Container, Grid, Typography, Box } from "@mui/material"
 
 export default function HomePage() {
     return (
-        <Box textAlign={"center"}>
-            <Typography variant={"body2"} mt={20} marginX={5}> <b>Welcome!!</b> to PERN CRUD To Do 📝 a space to allows you to plan your tasks</Typography>
+        <Box textAlign={"center"} marginX={5} >
+            <Typography variant={"body2"} mt={20}> <b>Welcome!!</b> to PERN CRUD To Do 📝 a space to allows you to plan your tasks</Typography>
+            <Box>
+                <Typography mt={5} variant="subtitle1">📅 Organize your task</Typography>
+                <Typography variant="subtitle1">✏️ Edit existing tasks easily</Typography>
+                <Typography variant="subtitle1">🗑️ Delete tasks you no longer need</Typography>
+                <Typography variant="subtitle1">📊 View your tasks in a clean interface</Typography>
+            </Box>
         </ Box>
     )
 }
