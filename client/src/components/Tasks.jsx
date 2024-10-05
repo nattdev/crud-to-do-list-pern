f60782f5111dc6ac5d105c41c0832477538ba3fb
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 21:24:13 -0500

    Implement DragAndDrop for Task Cards

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,23 +1,25 @@
 import { useEffect} from "react";
 import TaskCard from "./TaskCard.jsx";
 import { useTasks } from "./TaskContext.jsx";
-
+import { useDragAndDrop } from "@formkit/drag-and-drop/react";
 import { Box, Typography } from "@mui/material";
 
 export default function Tasks() {
 
     const { tasks, loadTasks } = useTasks();
+    const [parent, tasksDrag, setTasksDrag] = useDragAndDrop([]);
 
     useEffect(() => {
         loadTasks();
     }, []);
 
     useEffect(() => {
+      setTasksDrag(tasks)
     }, [tasks]);
-
+    
     const renderTasks = () => {
         if (tasks.length !== 0) {
-            return tasks.map((task) => (
+            return tasksDrag.map((task) => (
                 <TaskCard key={task.id} task={task} />
             ));
         } else {
@@ -26,11 +28,16 @@ export default function Tasks() {
     }
 
     return (
-        <Box mt={20}>
-            <Typography variant="h2" m={2}>TASKS</Typography>
-            <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-            {renderTasks()}
-            </Box>
+      <Box mt={6}>
+        <Typography variant="h2" m={2}>
+          TASKS
+        </Typography>
+        <Box
+			  ref={parent}
+          sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+        >
+          {renderTasks()}
         </Box>
+      </Box>
     );
 }
