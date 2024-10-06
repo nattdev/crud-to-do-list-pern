5c32972956016078cdf0863a151a6fb4b156d6a0
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-06 02:33:18 -0500

    Update styles to small devices

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -28,18 +28,18 @@ export default function TaskCard({ task }) {
     const label = { inputProps: { 'aria-label': 'Task Complete' } };
 
     return (
-        <Box border={1} m={2} p={2} sx={{maxWidth : {xs: 200, lg: 250}}}>
+        <Box border={1} m={2} p={2} sx={{maxWidth : {xs: "100%", lg: 250}, minWidth : {xs: 30, lg: 250}, width : {xs: 140, sm: 150}, flexGrow: 1, display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
             <Checkbox checked={taskComplete} onChange={() => setTaskComplete(!taskComplete)} {...label} color="success" />
             <Box sx={{textDecoration: taskComplete ? "line-through" : "none"}}>
-            <Box sx={{display: "flex", flexDirection: "column", gap: "1rem"}}>
-            <Typography variant="body1" sx={{textWrap: "nowrap"}}>Task #{task.id}</Typography>
-            <Typography  mb={1} sx={{textAlign: "left"}} variant="caption">{formattedDate}</Typography>
+            <Box sx={{display: "flex", flexDirection: "column", gap: "1rem", marginBottom: "auto"}}>
+            <Typography variant="body1" sx={{textWrap: "nowrap", fontSize: {xs: "1rem"}}}>Task #{task.id}</Typography>
+            <Typography  mb={{xs: 1}} sx={{textAlign: "left", fontSize: {xs: "0.7rem"}}} variant="caption">{formattedDate}</Typography>
             </Box>
-            <Typography sx={{fontSize: {xs: "1.5rem"}}} variant="h3">{task.title}</Typography>
-            <Typography sx={{fontSize: {xs: "1rem"}}} variant="body1" py={2}>{task.description}</Typography>
+            <Typography sx={{fontSize: {xs: "1rem"}, fontWeight: "500"}} variant="h3">{task.title}</Typography>
+            <Typography sx={{fontSize: {xs: "0.8rem"}}} variant="body1" py={2}>{task.description}</Typography>
             </Box>
-            <Stack direction="row" spacing={1}>
-                <Button variant="contained" onClick={handleClick}>EDIT</Button>
+            <Stack sx={{display : "flex"}} direction="row" spacing={1}>
+                <Button sx={{padding : "0.2px!important"}} variant="contained" onClick={handleClick}>EDIT</Button>
                 <ModalDelete task={task}/>
             </Stack>
         </Box>
